ee891a03f5cf8aecebc3fba5ae26c5cce2186b2e
conroy.whitney <conroy.whitney@e26fd1a8-e649-0410-ab16-2dcb40bf5c27>
2008-04-10 20:06:45 +0000

    changed lines in main to simply switch b/t test statements; fixed binOp error

--- trunk/code/Main.java
+++ trunk/code/Main.java
@@ -2,9 +2,8 @@
 public class Main {
 
 	public static void main(String[] args) {
-
-/*		
-		String test = "begin \n " +
+	
+		String daniel_complicated = "begin \n " +
 				"ID := 6;" +
 				"ID2 := asdfas;" +
 				"ID3:= 4+2; " +
@@ -13,28 +12,30 @@ public class Main {
 				"ID6 := ID3 * ID2; " +
 				"print ( ID4 + (4*ID5)) \n " +
 				"end";
-				
-				
-		String test = "begin ";
-		test += "print ( 4 ) \n ";
-		test += "end";
-
+						
+		String simple_print = "begin ";
+		simple_print += "print ( 4 ) \n ";
+		simple_print += "end";
 		
-		String test = "begin ";
-		test += "var1 := 6;";
-		test += "var2 := 4;";
-		test += "var3 := var1 + var2 ";
-		test += "end";
-
-*/
+		String simple_set = "begin ";
+		simple_set += "var1 := 6;";
+		simple_set += "var2 := 4;";
+		simple_set += "var3 := var1 + var2 ";
+		simple_set += "end";
 
 		String awesomeTest_fuckYeah = "begin ";
 		awesomeTest_fuckYeah += "awesome := sauce;";
 		awesomeTest_fuckYeah += "print(fuck, yeah);";
 		awesomeTest_fuckYeah += "meaning_of_life_the_universe_and_everything := 42 ";
 		awesomeTest_fuckYeah += "end";		
+			
+		String addTest = "begin ";
+		addTest += "ID1 := 3 + 4 ";
+		addTest += "end";
 		
-		Parser parser = new Parser(awesomeTest_fuckYeah);
+		String sTest = addTest;
+		
+		Parser parser = new Parser(sTest);
 		
 		TreeNode parserNodes = parser.microProgram();
 		
@@ -44,9 +45,7 @@ public class Main {
 		
 		//System.out.println(parser.isID("_some_Test2_"));
 
-        // create parser
-        // interpret parser output ??
-        // get and display tree ??		
+        System.out.println("Max Stack Size: " + parser.getMaxStackSize());
 
 	}
 
