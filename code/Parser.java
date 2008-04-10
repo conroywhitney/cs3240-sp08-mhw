a8588218ceaf2393c8aebf9c852b651dcf82ce97
conroy.whitney <conroy.whitney@e26fd1a8-e649-0410-ab16-2dcb40bf5c27>
2008-04-10 06:06:30 +0000

    added a good deal of comments; fixed the so-many-errors problem: binOp needed to be set to NULL after not matching for any of them (w00t)

--- code/Parser.java
+++ code/Parser.java
@@ -16,10 +16,12 @@ public class Parser {
     public TreeNode microProgram() {
     	System.out.println("\nmicroProgram");
     	
-    	TreeNode rootNode = new TreeNode(null, "microProgram");
+		// create original root node for Parse Tree
+    	TreeNode rootNode = new TreeNode("microProgram");
     	
     	rootNode.addChild(match("begin", true));
-                        
+        
+		// recursively add statementList and its children             
         rootNode.addChild(statementList());
         
         rootNode.addChild(match("end", true));
@@ -46,16 +48,24 @@ public class Parser {
     	System.out.println("\nstatementListPrime");
     	
     	TreeNode node = new TreeNode("statementListPrime");
-    		
+    	
+		// addChild returns TRUE if adding a child that is NOT NULL
+		// match() returns NULL if unable to match
         if (node.addChild(match(";"))) {
 			node.addChild(statementList());
         } else {
+			// using epsilon rule which ALWAYS means an error
+			// going to fast-forward to the end of the sentence: a ';' or 'end'
+
         	System.out.println("statementListPrime was EPSILON -- consuming tokens till ; or end");
         	
+			// 
         	String sCurrToken = this.tokenizer.next().getValue();
         	boolean bFirst = true;
         	while ((!sCurrToken.equals("end")) && (!sCurrToken.equals(";"))) {
         		if (bFirst) {
+					// only throw an error for the first problem token
+					// the other tokens in the sentence are just collatoral damage
         			error(this.tokenizer.next());
         			bFirst = false;
         		}
@@ -63,10 +73,13 @@ public class Parser {
         		if (this.tokenizer.hasNext()) {
         			sCurrToken = this.tokenizer.next().getValue();
         		} else {
+					// if we've not found "end" but we're at the end, get outa this loop (h4x)
         			break;
         		}
         	}
         	
+			// if we found a ';' and not 'end', move onto next sentence
+			// TODO: where is an error node actually created in this method ???
         	if (sCurrToken.equals(";")) {
         		node = statementListPrime();
         	} else {
@@ -84,7 +97,11 @@ public class Parser {
     	
     	TreeNode node = new TreeNode("statement");
     	
+		// test to see if we can match "print", continuing that rule if we can
         if (node.addChild(match("print"))) {
+			// if we've got "print", we REQUIRE "(", which is that second param
+			// match() will return an error node if we cannot match "("
+			// in which case addChild() will return FALSE
             if (node.addChild(match("(", true))) {
             	if (node.addChild(expList())) {
             		node.addChild(match(")", true));
@@ -96,9 +113,10 @@ public class Parser {
         	}
         } else {
         	// error
+        	node = new TreeNode(this.tokenizer.next(), "error");
         	error(this.tokenizer.next());
         	// TODO: do i need to consume ??
-        	node = new TreeNode(this.tokenizer.next(), "error");
+			// TODO: what about fast-forwarding ??
         }
         
         return node;
@@ -123,12 +141,13 @@ public class Parser {
     	System.out.println("\nexpListPrime");
     	
     	TreeNode node = new TreeNode("expListPrime");
-    	    	
+
+		// if we cannot match ',' then we ASSUME we go to EPSILON    	    	
         if (node.addChild(match(","))) {
             node.addChild(expList());
         } else {
         	System.out.println("expListPrime was EPSILON");
-        	// epsilon
+        	// epsilon a.k.a. NULL node
             node = null;
         }
         
@@ -136,25 +155,28 @@ public class Parser {
     }
 
     // <exp> -> ( <exp> ) <exp'>
-    // <exp> -> ID <exp'>
     // <exp> -> INTNUM <exp'>
+    // <exp> -> ID <exp'>
     public TreeNode exp() {
     	System.out.println("\nexp");
     	
     	TreeNode node = new TreeNode("exp");
     	    	
         if (node.addChild(match("("))) {
-        	if (node.addChild(exp()) & node.addChild(match(")", true))) {
-        		node.addChild(expPrime());
+			// this was originally a & b/t exp() and match()
+        	if (node.addChild(exp())) {
+				if (node.addChild(match(")", true))) {
+	        		node.addChild(expPrime());
+				}
         	}
         } else if (node.addChild(matchINTNUM())) {
             return expPrime();            
         } else if (node.addChild(matchID())) {
             return expPrime();
         } else {
+            node = new TreeNode(this.tokenizer.next(), "error");
         	error(this.tokenizer.next());
         	// TODO: do i need to consume ???
-            node = new TreeNode(this.tokenizer.next(), "error");
         }
         
         return node;
@@ -170,7 +192,7 @@ public class Parser {
         if (node.addChild(binOp())) {
             node.addChild(exp());
         } else {
-        	// because can go to epsilon, set to null
+        	// because can go to epsilon, set to NULL
         	node = null;
         	System.out.println("expPrime was EPSILON");
         }
@@ -192,6 +214,12 @@ public class Parser {
         if (!node.hasChildren()) { node.addChild(match("*")); }
         if (!node.hasChildren()) { node.addChild(match("+")); }
         if (!node.hasChildren()) { node.addChild(match("-")); }
+
+		// if after so many attempts, no children, steralize him/her
+		if (!node.hasChildren()) { node = null; }
+
+		// TODO: can we somehow use this short-circuit add to perform the RHS of
+		// supporting 5 * -5 ???? consuming two tokens at once ??
         
         return node;
     } 
@@ -200,30 +228,80 @@ public class Parser {
 		return match(s, false);
 	}
 
+	// this method will return a TreeNode with a KEYWORD token
+	// or NULL if the strings don't match
     public TreeNode match(String s, boolean required) {
     	System.out.println("match: " + s);
     	
-    	// this method will return a TreeNode (auto null)
     	TreeNode node = null;
     	
     	if (this.tokenizer.hasNext()) {
     		// get next token
     		Token t = this.tokenizer.next();
+
     		if (s.equals(t.getValue())) {
-    			// if our token is what we expect to see
-    			t.setType(Token.TokenType.KEYWORD);    	
-    			// create a new KEYWORD token		
+    			// if our token's value is what we expect to see
+
+				// ... let it forever be known it is a KEYWORD token (aka reserved characters)
+    			t.setType(Token.TokenType.KEYWORD);
+
+    			// create a new KEYWORD node for the tree (instead of it being NULL)		
     			node = new TreeNode(t, "keyword");
-    			// and consume the token from the tokenizer
+
+    			// consume the token from the tokenizer so that nobody sees this token again
 				this.tokenizer.consume();
+
     			System.out.println("consum'd: " + t.getValue());
     		} else {
-    			// not what we expected to see
+    			// our token's value was not what we expected to see
+				// either this was simply a test (like "herm, should i use the rule that starts with...")
+				// or it was REQUIRED, meaning this token NEEDS to have been found here
     			if (required) {
-    				// if this was REQUIRED, then give them an error token
-    				error(t);
-    				this.tokenizer.consume();
+    				// in such a REQUIRED case, give them back an error node w/ the affixed token
     				node = new TreeNode(t, "error");
+					// throw an error message
+    				error(t);
+					// and consume the token so nobody sees it evar again
+    				this.tokenizer.consume();
+    			}
+    		}
+    	}
+    	
+    	return node;
+    }
+	
+	// this function looks at a token and sees if it could be an ID
+	// if so, it returns an ID node to be added to the parse tree
+    public TreeNode matchID(boolean required) {
+    	System.out.println("matchID");
+    	
+    	TreeNode node = null;
+    	
+    	if (this.tokenizer.hasNext()) {
+			// look at next token in tokenizer
+    		Token t = this.tokenizer.next();
+
+    		if (this.isID(t.getValue())) {
+				// matches our "regex" (aka, character parsing) for an ID
+
+				// give the token type ID
+    			t.setType(Token.TokenType.ID);
+
+				// give the new node type ID, and the newfound ID token
+    			node = new TreeNode(t, "ID");
+
+				// consume token so nobody sees it again
+    			this.tokenizer.consume();
+
+    			System.out.println("consum'd: " + t.getValue());
+    		} else {
+				// this token did NOT match an ID -- either we were just fishing for options
+				// or we were REQUIRED to find an ID token here
+    			if (required) {
+					// give them back an error node if they were so sure of finding an ID here
+    				node = new TreeNode(t, "error");
+    				error(t);
+					// TODO: do we need to consume this now ?? methinks ...
     			}
     		}
     	}
@@ -231,33 +309,10 @@ public class Parser {
     	return node;
     }
 
+	// Overload for non-required
 	public TreeNode matchID() {
 		return matchID(false);
 	}
-	
-    public TreeNode matchID(boolean required) {
-    	System.out.println("matchID");
-    	
-    	// this method will return a TreeNode (auto null)
-    	TreeNode node = null;
-    	
-    	if (this.tokenizer.hasNext()) {
-    		Token t = this.tokenizer.next();
-    		if (this.isID(t.getValue())) {
-    			t.setType(Token.TokenType.ID);
-    			node = new TreeNode(t, "ID");
-    			this.tokenizer.consume();
-    			System.out.println("consum'd: " + t.getValue());
-    		} else {
-    			if (required) {
-    				error(t);
-    				node = new TreeNode(t, "error");
-    			}
-    		}
-    	}
-    	
-    	return node;
-    }
 
     public TreeNode matchINTNUM() {
     	System.out.println("matchINTNUM");
@@ -275,6 +330,7 @@ public class Parser {
     			//System.out.println("fux'd");
     		}
     	}
+
     	return node;
     }
     
@@ -333,8 +389,7 @@ public class Parser {
     }
     
     public boolean isInt(char c) {
-    	return ((c >= '0') &&
-    	(c <= '9'));
+    	return ((c >= '0') && (c <= '9'));
     }
     
     public boolean isAlpha(char c) {
