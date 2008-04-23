5b0b1f87218740121e53722ddf23a9bcca659419
conroy.whitney <conroy.whitney@e26fd1a8-e649-0410-ab16-2dcb40bf5c27>
2008-04-23 23:36:48 +0000

    have checking for undeclared variables

--- trunk/code/Parser.java
+++ trunk/code/Parser.java
@@ -8,21 +8,26 @@ public class Parser {
 
 	private TreeNode globalRootNode = null;
 	
+	private VariableList variableList = null;
+	
+	private boolean bRequireDeclaration = false;
+	
     public Parser(String sProgram) {
         this.tokenizer = new Tokenizer(sProgram);
         this.tokenizer.tokenize();
+        this.variableList = new VariableList();
     }
     
     private void error(Token t) {
     	System.out.println("\t\t=======================\n\t\tError on token: " + t.getValue() + "\n\t\t=======================");
-		////System.out.println("Tree So Far: ");
+		//System.out.println("Tree So Far: ");
 		//AbstractSyntaxTree tree = new AbstractSyntaxTree(globalRootNode);
-		////System.out.println(tree.getAST().toString());
+		//System.out.println(tree.getAST().toString());
     }
 
     // <Micro-program > -> begin <statement-list> end
     public TreeNode microProgram() {
-    	//System.out.println("\nmicroProgram");
+    	System.out.println("\nmicroProgram");
     	incrementStackCounter();
     	
 		// create original root node for Parse Tree
@@ -41,7 +46,7 @@ public class Parser {
 
     // <statement-list> -> <statement> <statement-list'>
     public TreeNode statementList() {
-    	//System.out.println("\nstatementList");
+    	System.out.println("\nstatementList");
     	incrementStackCounter();
     	    	
     	TreeNode node = new TreeNode("statementList");
@@ -57,7 +62,7 @@ public class Parser {
     // <statement-list'> -> ; <statement-list>
     // <statement-list'> -> \epsilon
     public TreeNode statementListPrime() {
-    	//System.out.println("\n\t=============statementListPrime================");
+    	System.out.println("\n\t=============statementListPrime================");
     	incrementStackCounter();
     	
     	TreeNode node = new TreeNode("statementListPrime");
@@ -104,7 +109,7 @@ public class Parser {
     // <statement> ->  print ( <exp-list> )
     // <statement> ->  ID := <exp>
     public TreeNode statement() {
-    	//System.out.println("\nstatement");
+    	System.out.println("\nstatement");
     	incrementStackCounter();
     	
     	TreeNode node = new TreeNode("statement");
@@ -115,13 +120,27 @@ public class Parser {
 			// match() will return an error node if we cannot match "("
 			// in which case addChild() will return FALSE
             if (node.addChild(match("(", true))) {
+            	// we are inside a print statement now
+            	// we need to make sure that all IDs used are declared already
+            	this.bRequireDeclaration = true;
+            	
             	if (node.addChild(expList())) {
             		node.addChild(match(")", true));
+            		
+            		// we are out of the print statement now so we can lax our guard
+            		this.bRequireDeclaration = false;
             	}
             }
         } else if (node.addChild(matchID())) {
         	if (node.addChild(match(":=", true))) {
+        		// we are on the right-side of an assignment operator now
+        		// so we need to make sure that all IDs are declared already
+        		this.bRequireDeclaration = true;
+        		
         		node.addChild(exp());
+        		
+        		// we are done with the assignment now so we can lax our guard
+        		this.bRequireDeclaration = false;
         	}
         } else {
         	// error
@@ -137,7 +156,7 @@ public class Parser {
 
     // <exp-list> -> <exp> <exp-list'>
     public TreeNode expList() {
-    	//System.out.println("\nexpList");
+    	System.out.println("\nexpList");
     	incrementStackCounter();
     	
     	TreeNode node = new TreeNode("expList");
@@ -150,10 +169,10 @@ public class Parser {
         return node;
     }
 
-    // <exp-list'> -> <exp-list>, <exp>
+    // <exp-list'> -> , <exp-list>
     // <exp-list'> -> \epsilon
     public TreeNode expListPrime() {
-    	//System.out.println("\nexpListPrime");
+    	System.out.println("\nexpListPrime");
     	incrementStackCounter();
     	
     	TreeNode node = new TreeNode("expListPrime");
@@ -172,7 +191,7 @@ public class Parser {
 
     // <exp> -> <addexp> <addexp'>
     public TreeNode exp() {
-    	//System.out.println("\nexp");
+    	System.out.println("\nexp");
     	incrementStackCounter();
     	
     	TreeNode node = new TreeNode("exp");
@@ -187,7 +206,7 @@ public class Parser {
 
     // <addexp> -> <mulexp> <mulexp'>
     public TreeNode addexp() {
-    	//System.out.println("\naddexp");
+    	System.out.println("\naddexp");
     	incrementStackCounter();
     	
     	TreeNode node = new TreeNode("addexp");
@@ -202,9 +221,9 @@ public class Parser {
 
     // <addexp'> -> + <exp>
     // <addexp'> -> - <exp>
-    // <addexp'> -> ε
+    // <addexp'> -> \epsilon
     public TreeNode addexpPrime() {
-    	//System.out.println("\naddexpPrime");
+    	System.out.println("\naddexpPrime");
     	incrementStackCounter();
     	
     	TreeNode node = new TreeNode("addexpPrime");
@@ -223,7 +242,7 @@ public class Parser {
 
     // <mulexp> -> <powexp> <powexp'>
     public TreeNode mulexp() {
-    	//System.out.println("\nmulexp");
+    	System.out.println("\nmulexp");
     	incrementStackCounter();
     	
     	TreeNode node = new TreeNode("mulexp");
@@ -237,9 +256,9 @@ public class Parser {
     }
 
     // <mulexp'> -> * <addexp>
-    // <mulexp'> -> ε
+    // <mulexp'> -> \epsilon
     public TreeNode mulexpPrime() {
-    	//System.out.println("\nmulexpPrime");
+    	System.out.println("\nmulexpPrime");
     	incrementStackCounter();
     	
     	TreeNode node = new TreeNode("mulexpPrime");
@@ -258,7 +277,7 @@ public class Parser {
     // <powexp> -> ID
     // <powexp> -> INTNUM
     public TreeNode powexp() {
-    	//System.out.println("\npowexp");
+    	System.out.println("\npowexp");
     	incrementStackCounter();
     	
     	TreeNode node = new TreeNode("powexp");
@@ -279,9 +298,9 @@ public class Parser {
     }
 
     // <powexp'> -> ** <mulexp>
-    // <powexp'> -> ε
+    // <powexp'> -> \epsilon
     public TreeNode powexpPrime() {
-    	//System.out.println("\npowexpPrime");
+    	System.out.println("\npowexpPrime");
     	incrementStackCounter();
     	
     	TreeNode node = new TreeNode("powexpPrime");
@@ -303,7 +322,7 @@ public class Parser {
 	// this method will return a TreeNode with a KEYWORD token
 	// or NULL if the strings don't match
     public TreeNode match(String s, boolean required) {
-    	//System.out.println("match: " + s);
+    	System.out.println("match: " + s);
     	
     	TreeNode node = null;
     	
@@ -314,7 +333,7 @@ public class Parser {
     		if (s.equals(t.getValue())) {
     			// if our token's value is what we expect to see
 
-                //System.out.println("\tconsume'd: " + s);
+                System.out.println("\tconsume'd: " + s);
 
 				// ... let it forever be known it is a KEYWORD token (aka reserved characters)
     			t.setType(Token.TokenType.KEYWORD);
@@ -346,7 +365,7 @@ public class Parser {
 	// this function looks at a token and sees if it could be an ID
 	// if so, it returns an ID node to be added to the parse tree
     public TreeNode matchID(boolean required) {
-    	//System.out.println("matchID");
+    	System.out.println("matchID");
     	
     	TreeNode node = null;
     	
@@ -356,19 +375,31 @@ public class Parser {
 
     		if (this.isID(t.getValue())) {
 				// matches our "regex" (aka, character parsing) for an ID
+				
+				// if we are within an assignment statement or a print statement
+				// we must make sure that this ID has already been declared
+				if ((this.bRequireDeclaration) && (!this.isIDDeclared(t.getValue()))) {
+					// we are using this ID in an illegal way
+					// we must turn this node into an error node
+					// and throw an error
+					node = new TreeNode(t, "error");
+					error(t);
+				} else {
+					// we are using this ID in a legal way
+					System.out.println("\tconsume'd: " + t.getValue());
 
-                //System.out.println("\tconsume'd: " + t.getValue());
-
-				// give the token type ID
-    			t.setType(Token.TokenType.ID);
-
-				// give the new node type ID, and the newfound ID token
-    			node = new TreeNode(t, "ID");
-
-				// consume token so nobody sees it again
-    			this.tokenizer.consume();
-
-    			//System.out.println("consum'd: " + t.getValue());
+					// give the token type ID
+	    			t.setType(Token.TokenType.ID);
+	
+					// give the new node type ID, and the newfound ID token
+	    			node = new TreeNode(t, "ID");
+	    			
+	    			// also, add this to our list of known variables
+	    			this.declare(t.getValue());
+	
+					// consume token so nobody sees it again
+	    			this.tokenizer.consume();				
+				}                
     		} else {
 				// this token did NOT match an ID -- either we were just fishing for options
 				// or we were REQUIRED to find an ID token here
@@ -389,14 +420,14 @@ public class Parser {
 	}
 
     public TreeNode matchINTNUM() {
-    	//System.out.println("matchINTNUM");
+    	System.out.println("matchINTNUM");
     	
     	TreeNode node = null;
     	
     	if (this.tokenizer.hasNext()) {
     		Token t = this.tokenizer.next();
     		if (this.isINTNUM(t.getValue())) {
-                //System.out.println("\tconsume'd: " + t.getValue());
+                System.out.println("\tconsume'd: " + t.getValue());
     			t.setType(Token.TokenType.INTNUM);
     			node = new TreeNode(t, "INTNUM");
     			this.tokenizer.consume();
@@ -404,6 +435,14 @@ public class Parser {
     	}
 
     	return node;
+    }
+    
+    private void declare(String sIDName) {
+    	this.variableList.update(new Variable(sIDName));	
+    }
+    
+    private boolean isIDDeclared(String sIDName) {
+    	return this.variableList.hasVariable(sIDName);
     }
     
     public boolean isID(String s) {
@@ -457,7 +496,7 @@ public class Parser {
 
 			// all other chars have to be ints
 			if ((i > 0) && !isInt(arr[i])) {
-				//System.out.println(i + ": " + arr[i] + "; !isInt");
+				System.out.println(i + ": " + arr[i] + "; !isInt");
     			b = false;
     		}
 
