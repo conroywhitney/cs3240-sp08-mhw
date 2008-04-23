211c5e27574ee1375c3b93c314bc0287c7eed68a
conroy.whitney <conroy.whitney@e26fd1a8-e649-0410-ab16-2dcb40bf5c27>
2008-04-23 15:13:25 +0000

    he added fileIO in addition to piping in

--- trunk/code/Main.java
+++ trunk/code/Main.java
@@ -1,38 +1,57 @@
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.*;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		String sInput = null;
-		String sLine = null;
+		BufferedReader br = null;
 
-		InputStreamReader isr = new InputStreamReader( System.in );
-		BufferedReader br = new BufferedReader( isr );
-
-		try {
-			while ((sLine = br.readLine()) != null) {
-				sInput += sLine.trim() + "\n";
+		if (args.length > 0) {
+			System.out.println(args[0]);
+			
+			try {
+				FileReader fr = new FileReader(args[0]);
+				br = new BufferedReader(fr);
+			} catch (IOException ioe) {
+				System.out.println("IO Error");
 			}
-			sInput = sInput.replace("nullbegin", "begin");
-		} catch (IOException ioe) {
+		} else {
+
+			InputStreamReader isr = new InputStreamReader(System.in);
+			br = new BufferedReader(isr);
+		}
+
+		if (br != null) {
+			String sInput = null;
+			String sLine = null;
+			try {
+				while ((sLine = br.readLine()) != null) {
+					sInput += sLine.trim() + "\n";
+				}
+				sInput = sInput.replace("nullbegin", "begin");
+			} catch (IOException ioe) {
+				System.out.println("IO Error");
+				sInput = null;
+			}
+
+			if (sInput != null) {
+				Parser parser = new Parser(sInput);
+
+				TreeNode parserNodes = parser.microProgram();
+
+				AbstractSyntaxTree tree = new AbstractSyntaxTree(parserNodes);
+				//System.out.println(tree.toString());
+				AbstractSyntaxTree ast = tree.getAST();
+				System.out.println(ast.toString());
+
+				System.out.println("Max Stack Size: "
+						+ parser.getMaxStackSize());
+				
+				System.out.println("Evaluation: " + ast.evaluate());
+			}
+		} else {
 			System.out.println("IO Error");
-			sInput = null;
 		}
-
-		if (sInput != null) {	
-			Parser parser = new Parser(sInput);
-		
-			TreeNode parserNodes = parser.microProgram();
-		
-			AbstractSyntaxTree tree = new AbstractSyntaxTree(parserNodes);
-			System.out.println(tree.getAST().toString());
-		
-		    System.out.println("Max Stack Size: " + parser.getMaxStackSize());
-		}
-
 	}
 
 }
