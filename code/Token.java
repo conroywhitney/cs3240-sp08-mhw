7305adf782ba6fdaebfd000546eebf3333da5cae
newatlanta19006 <newatlanta19006@e26fd1a8-e649-0410-ab16-2dcb40bf5c27>
2008-04-24 23:37:49 +0000

    Cleaned up code, fixed eval method, added options, system.outs

--- code/Token.java
+++ code/Token.java
@@ -1,28 +1,30 @@
 public class Token {
 
-    public enum TokenType { KEYWORD, ID, INTNUM, INVALID }
+	public enum TokenType {
+		KEYWORD, ID, INTNUM, INVALID
+	}
 
-    private String sValue;
-    private TokenType type;
+	private String sValue;
+	private TokenType type;
 
-    public Token(String sValue) {
-        this.sValue = sValue;
-    }
+	public Token(String sValue) {
+		this.sValue = sValue;
+	}
 
-    public String getValue() {
-        return this.sValue;
-    }
-    
-    public TokenType getType() {
-    	return this.type;
-    }
-    
-    public void setType(TokenType type) {
-    	this.type = type;
-    }
+	public String getValue() {
+		return this.sValue;
+	}
 
-    public int length() {
-        return this.sValue.length();
-    }
+	public TokenType getType() {
+		return this.type;
+	}
+
+	public void setType(TokenType type) {
+		this.type = type;
+	}
+
+	public int length() {
+		return this.sValue.length();
+	}
 
 }
--- code/TokenList.java
+++ code/TokenList.java
@@ -1,45 +1,41 @@
-
 public class TokenList {
 
 	private Token[] tokens;
-	
-    public TokenList() {
-    	this.tokens = new Token[0];
-    }
-    
-    public void add(Token t) {
+
+	public TokenList() {
+		this.tokens = new Token[0];
+	}
+
+	public void add(Token t) {
 		Token[] temp = new Token[this.tokens.length + 1];
 		for (int i = 0; i < this.tokens.length; i++) {
 			temp[i] = this.tokens[i];
-		}    	
-		
+		}
+
 		temp[this.tokens.length] = t;
 		this.tokens = temp;
-    }
-    
-    public void add(String sToken) {
-    	this.add(new Token(sToken));
-    }
-    
-    public void removeLast()
-    {
-    	if(tokens.length > 0)
-    	{
-    		Token[] temp = new Token[tokens.length - 1];
-    		for(int i = 0; i < temp.length; i++)
-    		{
-    			temp[i] = tokens[i];
-    		}
-    		tokens = temp;
-    	}
-    }
-    
-    public Token get(int i) {
-    	return this.tokens[i];
-    }
-    
-    public int size() {
-    	return this.tokens.length;
-    }
+	}
+
+	public void add(String sToken) {
+		this.add(new Token(sToken));
+	}
+
+	public void removeLast() {
+		if (tokens.length > 0) {
+			Token[] temp = new Token[tokens.length - 1];
+			for (int i = 0; i < temp.length; i++) {
+				temp[i] = tokens[i];
+			}
+			tokens = temp;
+		}
+	}
+
+	public Token get(int i) {
+		return this.tokens[i];
+	}
+
+	public int size() {
+		return this.tokens.length;
+	}
 
 }
