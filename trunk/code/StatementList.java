959a4473c15c3ed75e940a43dec700b5e550c96a
conroy.whitney <conroy.whitney@e26fd1a8-e649-0410-ab16-2dcb40bf5c27>
2008-04-26 15:52:30 +0000

    modified the parser to handle huge stacks; added statementlist to do this; getAST() still recursive so breaks

---
+++ trunk/code/StatementList.java
@@ -0,0 +1,33 @@
+import java.math.*;
+
+public class StatementList {
+
+	private TreeNode[] nodes;
+
+	public StatementList() {
+		this.nodes = new TreeNode[0];
+	}
+
+	public void add(TreeNode t) {
+		TreeNode[] temp = new TreeNode[this.nodes.length + 1];
+		for (int i = 0; i < this.nodes.length; i++) {
+			temp[i] = this.nodes[i];
+		}
+
+		temp[this.nodes.length] = t;
+		this.nodes = temp;
+	}
+
+	public TreeNode get(int i) {
+		return this.nodes[i];
+	}
+
+	public void set(int i, TreeNode t) {
+		this.nodes[i] = t;
+	}
+
+	public int size() {
+		return this.nodes.length;
+	}
+
+}
